0e772a942a2e6e89a52fdb1910987ae0493cfd63
LauraFranco404 <laurafranco1116@gmail.com>
2025-09-29 09:25:10 -0500

    melody and melodytrack

--- MusicProject/wsparams.lisp
+++ MusicProject/wsparams.lisp
@@ -1,1 +1,1 @@
-(in-package :om)(setf *ws-params* (list 1 (om-make-point 509 221) (om-make-point 1108 1100)))
+(in-package :om)(setf *ws-params* (list 1 (om-make-point 509 221) (om-make-point 1140 1100)))
